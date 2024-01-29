73b8d5a8a57af37df06bfa239f25723fe0d022f2
HAB <quentinboyer85@gmail.com>
2024-01-29 15:08:26 +0400

    -ajouté navbar

--- src/App.js
+++ src/App.js
@@ -5,19 +5,58 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+          <a class="navbar-brand" href="#">JOHN DOE</a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto"></ul>
+            <ul class="nav justify-content-end">
+              <li class="nav-item">
+                <a class="nav-link active" href="#">ACCUEIL</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">SERVICES</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">RÉALISATIONS</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">BLOG</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">ME CONTACTER</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
       </header>
+
+      <main className='App-main'>
+        <article className='main-presentation'>
+          <h1>Bonjour,je suis John Doe</h1>
+          <h2>Développeur web full stack</h2>
+          <button>En savoir plus</button>
+        </article>
+
+        <article className='main-about'>
+
+          
+
+        </article>
+
+
+      </main>
+
+
+
+
+
+
+
+
+
     </div>
   );
 }
